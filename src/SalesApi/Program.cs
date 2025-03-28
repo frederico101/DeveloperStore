4411889078948b6060624a6253f57bcda04f80a9
frederico101 <fredyalves.fredyalves@hotmail.com>
2025-03-28 13:15:41 -0300

    feat(docker): configure Docker Compose for gateway, sales-api, and SQL Server
    Added gateway service with port mapping 7777:7777 and dependency on sales-api.
    Configured sales-api service with port mapping 8090:8090 and connection to SQL Server using ConnectionStrings__SalesApiDb.
    Added db service using SQL Server 2019 image with port mapping 1433:1433.
    Connected all services to the evaluation-network.

--- src/SalesApi/Program.cs
+++ src/SalesApi/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.WebHost.UseUrls("http://localhost:8090");
+builder.WebHost.UseUrls("http://+:8090");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
